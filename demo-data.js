dfd17c9d8f0070bc9244f8fcf3f2980c956fbb14
provocandoquesuceda-ctrl <provocandoquesuceda@gmail.com>
2026-08-17 20:28:36 -0400

    Add demo data for CASO_SANTA case
    santa

---
+++ demo-data.js
@@ -0,0 +1,9 @@
+const CASO_SANTA = {
+  id: "SANTA-ARIAS-2026-001",
+  demandante: "SANTA ARIAS DE HERNANDEZ",
+  demandados: ["THERMA DE LEON", "DEYSIS RAMIREZ", "DIGNA MARTE"],
+  objeto: "Referimiento en desalojo + Ley 5869",
+  fechaAudiencia: "2026-08-04",
+  pruebas: ["Informe Agrimensor", "Acto Alguacil", "Testigo Iban Arias"],
+  escritos: ["Demanda", "Ampliatorio", "Poder", "Querella", "Deposito"]
+};
